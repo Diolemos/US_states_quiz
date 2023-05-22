82f780d3bd31bba9e83e86281fcaf12eae7dc781
Dio Lemos <pedro.devsc@outlook.com>
2023-05-21 22:18:38 -0300

    Create instance of screen

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.title("U.S States quiz game")
+image = 'blank_states_img.gif'
+screen.addshape(image)
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
