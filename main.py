059ba4a3bb34fb7acf90f75cfbb589a3c0f2d8d5
Dio Lemos <pedro.devsc@outlook.com>
2023-05-22 01:46:26 -0300

    add comments change main loop so game ends if user writes all the states

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ import pandas
 states_data = pandas.read_csv("50_states.csv")
 print(states_data)
 
+#set up screen
+
 screen = turtle.Screen()
 screen.title("U.S States quiz game")
 image = 'blank_states_img.gif'
 screen.addshape(image)
 correct_answers = []
-#Buggy library, how does this work :P a turtle has not even been instantiated 
+
 turtle.shape(image)
-while True:
+
+#main game loop
+
+
+while len(correct_answers)<50:
     answer_state = screen.textinput(title=f"{len(correct_answers)}/50", prompt="Pick a state name")
     answer_state = answer_state.title()
     states = states_data['state'].to_list()
-    # print(states)
-
+   
+    #if state name is entered again, ignore it
     if answer_state in states:
-        print("nailed!")
+        
         if answer_state not in correct_answers:
             correct_answers.append(answer_state)
-            #row needed to print the turtle(state name) 
+             
             data_row = states_data[states_data['state']== answer_state]  
             x_coor = int(data_row.x)
             y_coor = int(data_row.y)
@@ -32,7 +38,7 @@ while True:
             new_title.goto(x=x_coor,y=y_coor)
             new_title.write(arg=f"{answer_state}",align='center', font=('Arial',8, 'normal'))
            
-    # states_data[(states_data['state']).lower() == answer_state]
+   
 
 
 turtle.mainloop()
