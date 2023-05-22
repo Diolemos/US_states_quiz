a9fb289591e8f484b50573cb089df3a768c6010d
Dio Lemos <pedro.devsc@outlook.com>
2023-05-21 22:54:10 -0300

    import pandas create states_data variable

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from turtle import Turtle, Screen
+import turtle
+import pandas
 
-screen = Screen()
+states_data = pandas.read_csv("50_states.csv")
+print(states_data)
+
+screen = turtle.Screen()
 screen.title("U.S States quiz game")
 image = 'blank_states_img.gif'
 screen.addshape(image)
 
+#Buggy library, how does this work :P a turtle has not even been instantiated 
+turtle.shape(image)
 
+answer_state = screen.textinput(title="Guess the state", prompt="Pick a state name")
+answer_state = answer_state.lower()
+# print(answer_state)
 
-
-
-
-
-
-
-
-screen.exitonclick()
