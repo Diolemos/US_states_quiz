c19cbdf6375054effa04db925f1ac5ac259d97ba
Dio Lemos <pedro.devsc@outlook.com>
2023-05-22 00:57:15 -0300

    create correct answers list display in the prompt's title the user's progress

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ screen = turtle.Screen()
 screen.title("U.S States quiz game")
 image = 'blank_states_img.gif'
 screen.addshape(image)
-
+correct_answers = []
 #Buggy library, how does this work :P a turtle has not even been instantiated 
 turtle.shape(image)
+while True:
+    answer_state = screen.textinput(title=f"{len(correct_answers)}/50", prompt="Pick a state name")
+    answer_state = answer_state.title()
+    states = states_data['state'].to_list()
+    # print(states)
 
-answer_state = screen.textinput(title="Guess the state", prompt="Pick a state name")
-answer_state = answer_state.title()
-states = states_data['state'].to_list()
-# print(states)
+    if answer_state in states:
+        print("nailed!")
+        correct_answers.append(answer_state)
+    # states_data[(states_data['state']).lower() == answer_state]
 
-if answer_state in states:
-    print("nailed!")
-# states_data[(states_data['state']).lower() == answer_state]
+
+turtle.mainloop()
