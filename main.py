8b62dc5c7083a0c6d78f952e0c9f235d82a2cba4
Dio Lemos <pedro.devsc@outlook.com>
2023-05-22 21:27:22 -0300

    shorten 'exit' if statement by using list comprehension

--- main.py
+++ main.py
@@ -23,10 +23,7 @@ while len(correct_answers)<50:
     states = states_data['state'].to_list()
     
     if answer_state == 'Exit':
-        missing_states = []
-        for state in states:
-            if state not in correct_answers:
-                missing_states.append(state)
+        missing_states = [state for state in states if state not in correct_answers]
         new_data = pandas.DataFrame(missing_states)  
         new_data.to_csv("states_to_learn.csv")      
         break
